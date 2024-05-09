6a572fcfb414bf7a442c523b5cc862868fc73e59
Clarzeyn <wildan.mnf05@gmail.com>
2024-05-09 22:57:42 +0700

    :memo: add variable

---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var(
+	 name string
+	)
+	name = "Wildan Mochamad Nur Fallah"
+	fmt.Println(name)
+
+	name = "Clarzeyn"
+	fmt.Println(name)
+
+	var umur = 19;
+
+	fmt.Println(umur)
+}
